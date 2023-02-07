bfe6876480125b4c0a8ec28524054f20170ef42b
Arthur Guilherme Ribeiro Duarte <117129364+ArthurrGuilherme@users.noreply.github.com>
2023-02-07 14:01:46 -0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+/*Pre-loand*/
+function loading() {
+    document.getElementsByClassName('load')[0].style.display = "none";
+    document.getElementsByClassName('loandig')[0].style.display = "block";
+}
+
+/*Mapeamento*/ 
+document.querySelector("#serviços").addEventListener("click", function (event) {
+    event.preventDefault();
+
+    scrollTo("#mapa_servisos");
+
+    console.log("go to up");
+});
+
+function scrollTo(element) {
+    document.querySelector(element).scrollIntoView({ behavior: "smooth" });
+}
+
+document.querySelector("#contato").addEventListener("click", function (event) {
+    event.preventDefault();
+
+    scrollTo("#formulario");
+
+    console.log("go to up");
+});
+document.querySelector("#seta").addEventListener("click", function (event) {
+    event.preventDefault();
+
+    scrollTo("#header");
+
+    console.log("go to up");
+});
+
+/*Btn*/
+window.addEventListener('scroll', function () {
+    let scroll = document.querySelector('.scrolltop')
+        scroll.classList.toggle('active', window.scrollY > 200)
+})
+
