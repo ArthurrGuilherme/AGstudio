774751d794d0d3198b67731e0fea8f0712b0a7c6
Arthur Guilherme Ribeiro Duarte <117129364+ArthurrGuilherme@users.noreply.github.com>
2023-02-09 21:07:11 -0300

    Finalizado equipe

---
+++ js/main.js
@@ -0,0 +1,51 @@
+/*Pre-loand*/
+function loading() {
+    document.getElementsByClassName('load')[0].style.display = "none";
+    document.getElementsByClassName('loandig')[0].style.display = "block";
+}
+
+/*Mapeamento*/ 
+document.querySelector("#serviços").addEventListener("click", function (event) {
+    event.preventDefault();
+
+    scrollTo("#mapa_servisos");
+
+    console.log("go to up");
+});
+
+function scrollTo(element) {
+    document.querySelector(element).scrollIntoView({ behavior: "smooth" });
+}
+
+document.querySelector("#contato").addEventListener("click", function (event) {
+    event.preventDefault();
+
+    scrollTo("#formulario");
+
+    console.log("go to up");
+});
+document.querySelector("#seta").addEventListener("click", function (event) {
+    event.preventDefault();
+
+    scrollTo("#header");
+
+    console.log("go to up");
+});
+document.querySelector("#Equipe").addEventListener("click", function (event) {
+    event.preventDefault();
+
+    scrollTo("#container_equipe");
+
+    console.log("go to up");
+});
+
+/*Btn*/
+window.addEventListener('scroll', function () {
+    let scroll = document.querySelector('.scrolltop')
+        scroll.classList.toggle('active', window.scrollY > 200)
+})
+window.addEventListener("scroll", function () {
+  let scroll = document.querySelector(".scrollbottom")
+  scroll.classList.toggle("active", window.scrollY > 200)
+})
+
